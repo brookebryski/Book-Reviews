cd9aa83b11c387d0e5597b77f2ba85804dba5cb0
github username <github email address>
2020-12-09 16:33:38 -0800

    added source book

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :books, through :reviews
+    has_many :reviewed_books, through :reviews, source: :book 
 
     has_many :books
 end
