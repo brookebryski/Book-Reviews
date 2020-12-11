e396c8b3f5557c07f436f8328d0729c9ab4edc12
github username <github email address>
2020-12-11 14:57:45 -0800

    started create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,15 @@ class SessionsController < ApplicationController
 
     def destroy
         session.delete(:user_id)
-        redirect_to'/
+        redirect_to'/'
     end
     
     def new
     end
-    
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+    end
+
 end
