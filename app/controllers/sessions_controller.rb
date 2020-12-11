47d8a1f1fd4ed497a20f814311cad871c911405e
github username <github email address>
2020-12-11 15:31:24 -0800

    added bcrypt gem and finished sessions create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,13 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(username: params[:user][:username])
         if @user && @user.authenticate(params[:user][:password])
+        #if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Invalid login info. Please try again."
+            redirect_to login_path
+        end
     end
-
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :reviewed_books, through :reviews, source: :book 
+    has_many :reviewed_books, through: :reviews, source: :book 
 
     has_many :books
 
-    validates :username, uniqueness true, presence: true
+    validates :username, uniqueness: true, presence: true
 
     has_secure_password
 
