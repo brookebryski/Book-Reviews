f5c4d1afe4992723eb998b76849de56a75893d6e
github username <github email address>
2020-12-14 09:52:48 -0800

    create method in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,24 @@ class ReviewsController < ApplicationController
         @review = @book.reviews.build
     end
 
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+        redirect_to review_path(@review)
+        else
+          render :new
+        end
+    end
+
+
     def index
     end
+
+    private
+
+    def review_params
+        params.require(:review). permit(:book_id, :content, :stars, :title)
+    end
+
 end
+
