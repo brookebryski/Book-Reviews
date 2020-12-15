3b7250fd06086ec60773bfc1e262ad6ae1906637
github username <github email address>
2020-12-15 14:06:09 -0800

    added display header helper method for reviews

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,11 @@
 module ReviewsHelper
+
+    def display_header
+        if params[:book_id]
+            content_tag(:h1, "Add a Review for #{review.book.title} -  #{review.book.author.name}")
+        else
+          content_tag(:h1, "Write a review")
+        end
+    end
+
 end
