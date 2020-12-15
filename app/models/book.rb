179e8e5b92dbc6449a890349f9fb39069210baa4
github username <github email address>
2020-12-15 12:10:58 -0800

    only 1 review validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,6 +8,11 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validate :not_a_repeat
 
+  def author_attributes(attributes)
+    author = Author.find_or_create_by(attributes) if !attributes['name'].empty?
+  end
+
+
   def not_a_repeat
     book = Book.find_by(title: title, author_id: author_id)
     if !!book && book != self
