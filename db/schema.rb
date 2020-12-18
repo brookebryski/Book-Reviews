a6d777f1ed2ad48e1d320bf58010ffddb2c37f8f
github username <github email address>
2020-12-18 11:07:51 -0800

    added view for book edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_230224) do
+ActiveRecord::Schema.define(version: 2020_12_18_185943) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_12_09_230224) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "books", "authors"
