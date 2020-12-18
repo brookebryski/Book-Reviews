658e592252548df55ec9ac2b2b74b0960631f063
github username <github email address>
2020-12-18 12:58:50 -0800

    debugged syntax in nav method and current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+    helper_method :current_user, :logged_in?
+
     private
 
     def logged_in?
@@ -7,7 +9,7 @@ class ApplicationController < ActionController::Base
     end
 
     def current_user
-        @current_user ||= User.find_by_id(session[user_id])
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
 
     def redirect_if_not_logged_in
