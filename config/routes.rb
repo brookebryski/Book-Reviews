e540a4fbca6dfeae94cba944fdad197f8c56b280
github username <github email address>
2020-12-18 11:09:35 -0800

    added users show route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :index]
   end
   resources :authors
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+  
 end
