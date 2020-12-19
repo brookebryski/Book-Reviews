44910e8c0b8ae210c21d0b8f4a5b2ce4507c8d41
github username <github email address>
2020-12-19 10:25:10 -0800

    seeded sylvia plath

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,6 @@ kitty = User.create(username: "kitty", password: "kittycat", email: "kitty@cat.c
 
 paul = User.create(username: "paul", password: "paulanka", email: "paul@anka.com")
 
+plath = Author.create(name: "Sylvia Plath")
+
+belljar = Book.create(title: "The Bell Jar", description: "Sylvia Plath's shocking, realistic, and intensely emotional novel about a woman falling into the grip of insanity", genre: "Roman à clef", author_id: plath.id, user_id: kitty.id)
